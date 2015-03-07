199bc3828208b8dab0fcbdd6c947c520159682c1
Richard Schneider <richard@schneiderbox.net>
2015-03-07 09:40:54 -0600

    basic map generation

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var DISPLAY_WIDTH = 85;
+var DISPLAY_HEIGHT = 30;
+var DISPLAY_FONT_SIZE = 18;
+
+var WORLD_LEVELS = 10;
+
+// Set up display
+var display = new ROT.Display({
+	width: DISPLAY_WIDTH,
+	height: DISPLAY_HEIGHT,
+	fontSize: DISPLAY_FONT_SIZE
+});
+document.body.appendChild(display.getContainer());
+
+// Create world
+var mapData = []
+for(var level = 0; level < WORLD_LEVELS; level++){
+	mapData[level] = [];
+	
+	var map = new ROT.Map.Digger(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	map.create(function(x, y, wall){
+		if(!mapData[level][x]){
+			mapData[level][x] = [];
+		}
+		mapData[level][x][y] = wall;
+	});
+}
+
+// Draw
+for(var x = 0; x < DISPLAY_WIDTH; x++){
+	for(var y = 0; y < DISPLAY_HEIGHT; y++){
+		if(mapData[0][x][y]){
+			display.draw(x, y, '', '', '#333');
+		}else{
+			display.draw(x, y, '', '', '#111');
+		}
+	}
+}
