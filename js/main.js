8eefba31d2cf31434801af59a6f9a1245d178937
Richard Schneider <richard@schneiderbox.net>
2015-03-12 16:50:29 -0500

    add intro screen

--- js/main.js
+++ js/main.js
@@ -7,67 +7,95 @@ window.controller = undefined;
 window.scheduler = undefined;
 window.log = undefined;
 window.partyStatus = undefined;
+window.redraw = undefined;
 
-// Display
-display = new ROT.Display({
-	width: DISPLAY_WIDTH,
-	height: DISPLAY_HEIGHT,
-	fontSize: DISPLAY_FONT_SIZE
-});
-document.body.appendChild(display.getContainer());
+function main(){
+	var names = [];
+	var classes = [];
+	for(var i = 0; i < 4; i++){
+		names.push(document.getElementById('name'+i).value);
+		var className = document.getElementById('class'+i).value;
+		var cls;
+		switch(className){
+			case 'Cleric':
+				cls = Cleric;
+				break;
+			case 'Fighter':
+				cls = Fighter;
+				break;
+			case 'Mage':
+				cls = Mage;
+				break;
+			case 'Rogue':
+				cls = Rogue;
+				break;
+		}
+		classes.push(cls);
+	}
 
-// World
-world = new World();
+	document.body.innerHTML = '';
 
-// Controller
-controller = new Controller();
-
-// Scheduler
-scheduler = new ROT.Scheduler.Simple();
-
-// UI elements
-log = new Log();
-partyStatus = new PartyStatus();
-
-// Create PCs
-// TODO stub
-var startX = world.maps[0].stairUp.x;
-var startY = world.maps[0].stairUp.y;
-var startXs = [startX, startX, startX - 1, startX + 1];
-var startYs = [startY - 1, startY + 1, startY, startY];
-
-new Fighter('Jack', startXs[0], startYs[0]);
-new Mage('Rufus', startXs[1], startYs[1]);
-new Rogue('Zachary', startXs[2], startYs[2]);
-new Cleric('Percival', startXs[3], startYs[3]);
-
-spawnBlobs();
-spawnBeetles();
-
-partyStatus.draw();
-
-// Function to redraw the whole screen
-function redraw(z){
-	display.clear();
-	world.draw();
-	log.draw();
-	partyStatus.draw();
-}
-
-// Game loop
-function frame(){
-	var entity = scheduler.next();
-	entity.active = true;
-
-	// Draw
-	world.draw(entity.z)
-
-	entity.turn(function(){
-		entity.active = false;
-		frame();
+	// Display
+	display = new ROT.Display({
+		width: DISPLAY_WIDTH,
+		height: DISPLAY_HEIGHT,
+		fontSize: DISPLAY_FONT_SIZE
 	});
+	document.body.appendChild(display.getContainer());
+
+	// World
+	world = new World();
+
+	// Controller
+	controller = new Controller();
+
+	// Scheduler
+	scheduler = new ROT.Scheduler.Simple();
+
+	// UI elements
+	log = new Log();
+	partyStatus = new PartyStatus();
+
+	// Create PCs
+	// TODO stub
+	var startX = world.maps[0].stairUp.x;
+	var startY = world.maps[0].stairUp.y;
+	var startXs = [startX, startX, startX - 1, startX + 1];
+	var startYs = [startY - 1, startY + 1, startY, startY];
+	
+	new classes[0](names[0], startXs[0], startYs[0]);
+	new classes[1](names[1], startXs[3], startYs[3]);
+	new classes[2](names[2], startXs[1], startYs[1]);
+	new classes[3](names[3], startXs[2], startYs[2]);
+
+	spawnBlobs();
+	spawnBeetles();
 
 	partyStatus.draw();
-}
 
-frame();
+	// Function to redraw the whole screen
+	redraw = function(z){
+		display.clear();
+		world.draw();
+		log.draw();
+		partyStatus.draw();
+	}
+
+	// Game loop
+	function frame(){
+		var entity = scheduler.next();
+		entity.active = true;
+
+		// Draw
+		world.draw(entity.z)
+
+		entity.turn(function(){
+			entity.active = false;
+			frame();
+		});
+
+		partyStatus.draw();
+	}
+
+	frame();
+}
