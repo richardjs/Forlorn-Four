c4a54758b36653e6b1615b9aed085896b847e12e
Richard Schneider <richard@schneiderbox.net>
2015-03-12 23:23:23 -0500

    reposition log

--- js/ui/log.js
+++ js/ui/log.js
@@ -2,7 +2,7 @@
 
 function Log(){
 	this.left = WORLD_WIDTH;
-	this.top = Math.floor(WORLD_HEIGHT * 2/3);
+	this.top = 15;
 	this.width = DISPLAY_WIDTH - this.left;
 	this.height = DISPLAY_HEIGHT - this.top;
 
