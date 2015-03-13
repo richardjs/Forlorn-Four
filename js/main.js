89f574801cb0051c164c8dd5672910a41e90a0e1
Richard Schneider <richard@schneiderbox.net>
2015-03-12 23:59:43 -0500

    add dragons

--- js/main.js
+++ js/main.js
@@ -80,6 +80,7 @@ function main(){
 	spawnBeetles();
 	spawnBlobs();
 	spawnCentipedes();
+	spawnDragon();
 	spawnSpiders();
 	spawnTrolls();
 
---
+++ js/mobs/dragon.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function spawnDragon(){
+	var count = Math.floor(ROT.RNG.getNormal(5, 2));
+	for(var i = 0; i < count; i++){
+		var z;
+		do{
+			z = Math.floor(ROT.RNG.getNormal(9, 1));
+		}while(z < 0 || z > WORLD_LEVELS - 1);
+		var pos = world.findOpenSpace(z);
+		new Dragon(pos.x, pos.y, z);
+	}
+}
+
+function Dragon(x, y, z){
+	AgroMob.call(this, 'dragon', x, y, z, 'D', '#800', 80, 100, 200);
+}
+
+Dragon.prototype = Object.create(AgroMob.prototype);
